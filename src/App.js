e6974c4a3c665ddc8acb8bbafdfe31b5bb4a0ebf
Steven Krenn <stevekrenn@gmail.com>
2023-04-30 19:38:23 -0500

    added component extension, componentDidMount, states, starting w/ input on UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
+import { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+    console.log('constructor');
+  };
+
+  componentDidMount() {
+    //componentDidMount: Used when a class component needs to leverage some kind of API call to get data it needs to display appropriate UI
+    console.log('componentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) =>
+        this.setState(
+          //Makes sure component updates
+          () => {
+            return { monsters: users }
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    console.log('render')
+    return <div className="App">
+
+
+      <input className='search-box' type='search' placeholder='Search Monsters...' />
+
+      {this.state.monsters.map((monster) => {
+        return <div key={monster.id}>
+          <h1>{monster.name}</h1>
+        </div>;
+      },
+        () => {
+          console.log(this.state)
+        }
+      )}
+
+
+
+
+    </div>;
+  };
 }
 
 export default App;
