4a482de6eacb3be3b16da60263738020341c5297
Steven Krenn <stevekrenn@gmail.com>
2023-05-11 15:00:12 -0500

    added monster API call to useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // import CardList from './components/card-list/card-list.component'
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
   const [searchField, setSearchField] = useState(''); // [value, setValue]
+  const [monsters, setMonsters] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => setMonsters(users)
+      );
+  }, [])
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase()
     setSearchField(searchFieldString)
   };
+
+  const filteredMonsters = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
 
   return (
     <div className="App" >
@@ -21,7 +33,7 @@ const App = () => {
         placeholder='Search monsters...'
       />
 
-      {/* <CardList monsters={filteredMonsters} /> */}
+      <CardList monsters={filteredMonsters} />
     </div >
   )
 }
@@ -36,23 +48,11 @@ const App = () => {
 //     };
 //   };
 
-//   componentDidMount() {
-//     //componentDidMount: Used when a class component needs to leverage some kind of API call to get data it needs to display appropriate UI
-//     console.log('componentDidMount')
-//     fetch('https://jsonplaceholder.typicode.com/users')
-//       .then(response => response.json())
-//       .then((users) =>
-//         this.setState(
-//           //Makes sure component updates
-//           () => {
-//             return { monsters: users }
-//           },
-//           () => {
-//             console.log(this.state);
-//           }
-//         )
-//       );
-//   }
+// componentDidMount() {
+//   //componentDidMount: Used when a class component needs to leverage some kind of API call to get data it needs to display appropriate UI
+//   console.log('componentDidMount')
+
+// }
 
 // onSearchChange = (event) => {
 //   const searchField = event.target.value.toLowerCase()
@@ -64,11 +64,6 @@ const App = () => {
 //   render() {
 //     const { monsters, searchField } = this.state;
 //     const { onSearchChange } = this;
-
-//     const filteredMonsters = monsters.filter(monster =>
-//       monster.name.toLowerCase().includes(searchField.toLowerCase())
-//     );
-
 
 //     return (
 //       // <div className="App" >
