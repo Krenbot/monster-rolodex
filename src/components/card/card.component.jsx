7920ffb5250e527b112e83608ac5ee0f58c41db9
Steven Krenn <stevekrenn@gmail.com>
2023-05-14 11:55:39 -0500

    edits on functional Card component, destructuring values

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,14 +1,19 @@
 import './card.styles.css';
 
-export const Card = props => (
-    <div className='card-container'>
-        <img
-            alt='monster'
-            src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
-        />
-        <h2> {props.monster.name} </h2>
-        <p> {props.monster.email} </p>
-    </div>
-);
+const Card = ({ monster }) => {
 
-export default Card
+    const { id, name, email } = monster;
+
+    return (
+        <div className='card-container' key={id}>
+            <img
+                alt='monster'
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+            />
+            <h2> {name} </h2>
+            <p> {email} </p>
+        </div>
+    );
+};
+
+export default Card;
