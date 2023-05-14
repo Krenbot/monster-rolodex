5deb19cbb892b62ae6013d68ae8f4d10ef131570
Steven Krenn <stevekrenn@gmail.com>
2023-05-14 11:35:39 -0500

    deleted testing string firing code/console logs, deleted old class syntax

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const App = () => {
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState([]);
-  const [stringField, setStringField] = useState('');
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -28,10 +27,6 @@ const App = () => {
     setSearchField(searchFieldString)
   };
 
-  const onStringChange = (event) => {
-    setStringField(event.target.value);
-  }
-
   return (
     <div className="App" >
       <h1 className='app-title'>Monsters Rolodex</h1>
@@ -47,44 +42,4 @@ const App = () => {
   )
 }
 
-// class App extends Component {
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       monsters: [],
-//       searchField: ''
-//     };
-//   };
-
-// componentDidMount() {
-//   //componentDidMount: Used when a class component needs to leverage some kind of API call to get data it needs to display appropriate UI
-//   console.log('componentDidMount')
-
-// }
-
-// onSearchChange = (event) => {
-//   const searchField = event.target.value.toLowerCase()
-//   this.setState(() => {
-//     return { searchField };
-//   });
-// };
-
-//   render() {
-//     const { monsters, searchField } = this.state;
-//     const { onSearchChange } = this;
-
-//     return (
-//       // <div className="App" >
-//       //   <h1 className='app-title'>Monsters Rolodex</h1>
-//       //   <SearchBox
-//       //     className='search-box'
-//       //     onChangeHandler={onSearchChange} placeholder='Search monsters...'
-//       //   />
-
-//       //   <CardList monsters={filteredMonsters} />
-//       // </div >
-//     );
-//   }
-// }
 export default App;
